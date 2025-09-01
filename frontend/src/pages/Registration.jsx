174dc67987cd3da41df83ab930faebd342a8325a
Sooraj Kumar Gupta <162137028+SoorajGupta10@users.noreply.github.com>
2025-09-01 15:13:59 +0530

    Update Registration.jsx

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -6,7 +6,7 @@ import { MdOutlineRemoveRedEye } from "react-icons/md";
 import { IoEyeSharp } from "react-icons/io5";
 import { useState } from "react";
 import { useContext } from "react";
-import { authDataContext } from "../context/authContext";
+import { authDataContext } from "../context/AuthContext";
 import axios from "axios";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../utils/Firebase";
